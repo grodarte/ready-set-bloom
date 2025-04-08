e4441c20f6b95db77c697da4c8f387e799ee9c22
Gabrielle <ms.grodarte@gmail.com>
2025-04-08 14:06:20 -0700

    will initialize react router

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
-import App from "./components/App";
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter , RouterProvider } from 'react-router-dom';
 import "./index.css";
-import { createRoot } from "react-dom/client";
+import routes from './routes';
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+const router = createBrowserRouter(routes)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<RouterProvider router={router}/>);
--- server/app.py
+++ server/app.py
@@ -60,18 +60,36 @@ class FlowerResource(Resource):
 class RibbonResource(Resource):
 
     def get(self):
-        pass
+        ribbon_dicts = [ribbon.to_dict() for ribbon in Ribbon.query.all()]
+        return ribbon_dicts , 200
 
     def post(self):
-        pass
+        json = request.get_json()
+        try:
+            new_ribbon = Ribbon(color=json['color'])
+            db.session.add(new_ribbon)
+            db.session.commit()
+
+            return new_ribbon.to_dict() , 201
+        except:
+            return {"errors":["validation errors"]} , 400
 
 class AccentResource(Resource):
 
     def get(self):
-        pass
+        accent_dicts = [accent.to_dict() for accent in Accent.query.all()]
+        return accent_dicts , 200
 
     def post(self):
-        pass
+        json = request.get_json()
+        try:
+            new_accent = Accent(color=json['color'])
+            db.session.add(new_accent)
+            db.session.commit()
+
+            return new_accent.to_dict() , 201
+        except:
+            return {"errors":["validation errors"]} , 400
 
 api.add_resource(Home, '/api')
 api.add_resource(WristletResource, '/api/wristlets')
