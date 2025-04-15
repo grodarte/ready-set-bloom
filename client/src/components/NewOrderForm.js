c16baa2ca0b5329bc47eec7c39a057c66d32bffc
Gabrielle <ms.grodarte@gmail.com>
2025-04-15 13:21:27 -0700

    will init formik and yup for new orders

--- client/src/components/NewOrderForm.js
+++ client/src/components/NewOrderForm.js
@@ -3,19 +3,46 @@ import * as yup from 'yup';
 
 function NewOrderForm() {
 
+    const orderSchema = yup.object().shape({
+        name: yup.string().required("Required"),
+        phone: yup.string().required("Required").matches(/^1?\d{10}$/, "Phone number must be 10 digits (optionally starting with a 1)"),
+        address: yup.string().required("Required"),
+        delivery_details: yup.string(),
+        event_id: yup.number().required("Required") // .typeError("Must select an event") before required
+    })
+
     const formik = useFormik({
         initialValues: {
-
+            name: "",
+            phone: "",
+            address: "",
+            delivery_details: "",
+            event_id: ""
         },
-        onSubmit: values => {
-            alert()
+        validationSchema: orderSchema,
+        onSubmit: (values) => {
+            fetch('/api/orders', {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(values)
+            })
+            .then(r=>r.json())
+            .then(newOrder => {
+                // sets new order in global state 
+            })
         }
     })
     
     return (
         <div>
             <h1>Add New Order</h1>
-            <form action=""></form>
+            <form onSubmit={formik.handleSubmit}>
+                <input 
+                    type="text"
+                />
+            </form>
         </div>
     )
 }
