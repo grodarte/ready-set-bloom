75606a5d6ddc630fc88bac4547469d9b73a2dcfa
Gabrielle <ms.grodarte@gmail.com>
2025-04-22 15:13:53 -0700

    will style navbar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,7 +7,6 @@ function App() {
   return (
     <div>
       <header>
-        <h1>Ready Set Bloom</h1>
         <NavBar/>
       </header>
       <ContextProviderWrapper>
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,28 +1,16 @@
 import { NavLink } from "react-router-dom";
+import "../css/navbar.css";
 
 function NavBar() {
     return (
-        <nav>
-            <NavLink
-                to="/"
-            >
-                Dashboard
-            </NavLink>
-            <NavLink
-                to="/orders"
-            >
-                Orders
-            </NavLink>
-            <NavLink
-                to="/items"
-            >
-                Items
-            </NavLink>
-            <NavLink
-                to="/neworderform"
-            >
-                + New Order
-            </NavLink>
+        <nav className="navbar">
+            <h1 className="navbar-title">Ready Set Bloom</h1>
+            <div className="navbar-links">
+                <NavLink to="/" className="nav-link">Dashboard</NavLink>
+                <NavLink to="/orders" className="nav-link">Orders</NavLink>
+                <NavLink to="/items" className="nav-link">Items</NavLink>
+                <NavLink to="/neworderform" className="nav-link new-order">+ New Order</NavLink>
+            </div>
         </nav>
     )
 }
