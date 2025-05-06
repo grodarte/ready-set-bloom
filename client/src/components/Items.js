eb728269c58107ba54e673a9a88557875ea0e43d
Gabrielle <ms.grodarte@gmail.com>
2025-05-06 13:41:51 -0700

    adds filter to items

--- client/src/components/Items.js
+++ client/src/components/Items.js
@@ -1,21 +1,35 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ItemContext } from "../context/item";
+import { DateContext } from "../context/DateContext";
 import Item from "./Item";
+import StatusFilter from "./StatusFilter";
 import "../styles/items.css";
 
 function Items() {
     const { items } = useContext(ItemContext)
+    const { startOfWeek, endOfWeek, startOfNextWeek, endOfNextWeek } = useContext(DateContext)
+    const [filter, setFilter] = useState("this_week")
 
+    const filteredItems = items.filter(item => {
+        if (filter === 'all') return true
+        if (filter === 'active') return item.order.status !== "completed"
+        if (filter === 'completed') return item.order.status === 'completed'
+        if (filter === 'this_week') {
+            const itemDate = new Date(item.order.event.event_date)
+            return itemDate >= startOfWeek && itemDate <= endOfWeek
+        }
+    })
     
     if (!items) return <p>Loading items...</p>
     
-    const itemElements = items.map(item=>{
+    const itemElements = filteredItems.map(item=>{
         return <Item key={item.id} item={item}/>
     })
     
     return (
         <div className="items-container">
             <h1 className="items-heading">Items</h1>
+            <StatusFilter currentFilter={filter} setFilter={setFilter}/>
             <table className="items-table">
                 <thead>
                     <tr>
